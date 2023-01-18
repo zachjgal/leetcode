c277e20d169a4240a55221f34b402f44c502b842
Zachary Galeaz <zachjgal@gmail.com>
2023-01-18 16:47:46 -0500

    add two sum

---
+++ easy/1_two_sum.py
@@ -0,0 +1,9 @@
+from typing import List, Tuple
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> Tuple[int, int]:
+        m = {}
+        for i, n in enumerate(nums):
+            if target - n in m:
+                return m[target - n], i
+            m[n] = i
+
